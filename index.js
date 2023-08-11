0b87ca86e61f02eae5c0e140184d51e314812da8
Kenneth <kvmoore097@gmail.com>
2023-08-10 19:34:38 -0500

    Phase 1 strings Rev lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedCurrentUser = "GRACE HOPPER";
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${excitedCurrentUser}!`;
+
+
+const firstInitial = currentUser[0];
+
+const restOfName = currentUser.slice(1);
+
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
